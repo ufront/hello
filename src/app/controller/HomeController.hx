cd37a25287702ba97f076c75074a696bf4e3d584
Jason O'Neil <jason.oneil@gmail.com>
2015-03-30 12:51:07 +0800

    Don't use ufadmin on the client.

--- src/app/controller/HomeController.hx
+++ src/app/controller/HomeController.hx
@@ -3,35 +3,39 @@ package app.controller;
 import ufront.web.Controller;
 import app.api.SignupApi;
 import ufront.web.result.*;
-import ufront.ufadmin.controller.UFAdminHomeController;
 import app.Permissions;
+#if server
+	import ufront.ufadmin.controller.UFAdminHomeController;
+#end
 
 class HomeController extends Controller {
 
 	@inject
 	public var signupApi:SignupApi;
-	
-	@:route("/ufadmin/*")
-	public var ufAdminController:UFAdminHomeController;
-	
-	@:route(GET, "/$name")
+
+	#if server
+		@:route("/ufadmin/*")
+		public var ufAdminController:UFAdminHomeController;
+	#end
+
+	@:route(GET, "$name")
 	public function homepage( ?name:String="World" ) {
 		ufTrace( 'Hey $name, did you know you can trace straight to the browser console?' );
 		return new ViewResult({ title: 'Hello $name' });
 	}
-	
+
 	@:route(POST, "/subscribe")
 	public function signup( args:{ name:String, email:String } ) {
 		signupApi.registerEmail( args.name, args.email );
 		var name = (args.name!=""&&args.name!=null) ? args.name : "World";
 		return new RedirectResult( '/thank/$name' );
 	}
-	
+
 	@:route(GET, "/thank/$name")
 	public function thankyou( name:String ) {
 		return new ViewResult({ title: 'Thanks $name!' });
 	}
-	
+
 	@:route("/subscribers/list.csv")
 	public function listSubscribers() {
 		context.auth.requirePermission( CanViewSubscriberList );
